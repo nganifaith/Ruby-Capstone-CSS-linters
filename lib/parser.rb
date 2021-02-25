ce0bd61d3243d5e386bd27e9cadd46e89077487f
nganifaith <nganifaith@gmail.com>
2021-02-25 21:54:16 +0100

    implement identation check using block defination

--- lib/parser.rb
+++ lib/parser.rb
@@ -22,7 +22,7 @@ class Parser
   end
 
   def check_comment
-    @open_comment = @current_line.include?('/*') unless open_comment
+    @open_comment = @current_line.include?('/*')
     @open_comment &&= !@current_line.include?('*/')
     @open_comment || @current_line.strip.start_with?('*/') || @current_line.strip.start_with?('*/')
   end
@@ -32,14 +32,7 @@ class Parser
   end
 
   def check_block
-    @open_blocks += 1 if @current_line.include?('{')
-    if @current_line.include?('{') && !@current_line.include?(' {')
-      error_message('Expected space before {', 'warning')
-    end
-    if @current_line.include?('}')
-      @open_blocks.positive? ? @open_blocks -= 1 : error_message('Stray closing }','error')
-      @closed_block = true
-    end
+    check_close_block(check_open_block)
   end
 
   def check_new_line
@@ -52,8 +45,11 @@ class Parser
 
   def remove_comment
     start_index = @current_line.index('/*')
-    if start_index.nil?
-      @current_line = !@current_line.include?('*/') ? @current_line[0...start_index] : @current_line[0...start_index] + @current_line[@current_line.index('*/') + 2...@current_line.length]
+    stop_index = @current_line.index('*/')
+    unless start_index.nil?
+      first_half = @current_line[0...start_index]
+      !stop_index.nil? ? second_half = @current_line[stop_index + 2...@current_line.length] : second_half =  ''
+      @current_line = first_half + second_half
     end
   end
 
@@ -61,17 +57,43 @@ class Parser
     return unless @current_line.strip.length.positive?
 
     matches = @current_line.scan(/[;{}]/)
-    if matches.length > 1 || (matches.length == 1 && @current_line.strip.index(matches[0]) != @current_line.strip.length - 1)
+    if matches.length > 1 || (matches.length == 1 && !@current_line.strip.end_with?(matches[0]))
       error_message("Expected new line after #{matches[0]}", 'error')
-
     elsif matches.empty? && !@current_line.strip.end_with?(',')
       error_message('Missing either a ; { or }'.colorize(:blue), 'error')
     end
   end
 
+  def check_indentation
+    expect_space = @open_blocks * @indent_size
+    current_space = @current_line[/\A */].size
+    return unless current_space != expect_space
+
+    error_message("Expected #{expect_space} spaces but got #{current_space}".colorize(:light_blue), 'error')
+  end
+  
   private
 
   def error_message(message, severity)
     @errors.push(ErrorFile.new(@current_line_index, message, severity))
+  end
+
+  def check_open_block
+    return unless @current_line.include?('{')
+
+    check_indentation # check for indentation before opening the block
+    @open_blocks += 1
+    error_message('Expected space before {', 'warning') unless @current_line.include?(' {')
+    true
+  end
+
+  def check_close_block(checked)
+    return unless @current_line.include?('}')
+
+    @open_blocks.positive? ? @open_blocks -= 1 : error_message('Stray closing }', 'error')
+    @closed_block = true
+    # check for indentaion after closing the block if  it has not yet been 
+    # checked (single line blocks)
+    check_indentation unless checked
   end
 end
