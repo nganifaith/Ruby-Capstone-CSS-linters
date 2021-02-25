72d57059d7afa90915d72aeb3c3dae5479fb3824
nganifaith <nganifaith@gmail.com>
2021-02-25 08:00:39 +0100

    add private method for error messages

--- lib/parser.rb
+++ lib/parser.rb
@@ -2,32 +2,46 @@ require_relative 'error_file.rb'
 
 
 class Parser
-  attr_reader :errors, :open_blocks, :open_comment, :indent_size, :num_selectors
+  attr_reader :errors, :open_blocks, :open_comment, :indent_size, :num_selectors, :current_line, :current_line_index
   def initialize
     @errors = []
     @open_blocks = 0
     @open_comment = false
     @indent_size = 2
     @num_selectors = 0
+    @current_line_index = 1
+    @current_line = ''
   end
 
-  def check_comment(line, line_ind)
+  def check_comment
     unless open_comment
-      @open_comment = line.include?("/*")
+      @open_comment = @current_line.include?("/*")
     end
-    @open_comment = @open_comment && !line.include?("*/")
-    @open_comment || line.strip.start_with?("/*")
+    @open_comment = @open_comment && !@current_line.include?("*/")
+    @open_comment || @current_line.strip.start_with?("/*")
   end
 
-  def trailing_space(line, line_ind)
-    @errors.push(ErrorFile.new(line_ind, 'Trailing space', 'warning'))  if line.end_with?(" ")
+  def trailing_space
+    error_message('Trailing space', 'warning')  if @current_line.end_with?(" ")
   end
 
-  def check_block(line, line_ind)
-    @open_blocks += 1 if line.include?("{")
-    @errors.push(ErrorFile.new(line_ind, 'Expected space before {', 'warning')) if line.include?('{') && !line.include?(' {')
-    if line.include?('}')
-      @open_blocks.positive? ? @open_blocks -= 1 : @errors.push(ErrorFile.new(line_ind, 'Stray closing }', 'error'))
+  def check_block
+    @open_blocks += 1 if @current_line.include?("{")
+    error_message('Expected space before {', 'warning') if @current_line.include?('{') && !@current_line.include?(' {')
+    if @current_line.include?('}')
+      @open_blocks.positive? ? @open_blocks -= 1 : error_message('Stray closing }', 'error')
     end
   end
+
+  def next_line(line) 
+    @current_line_index += 1
+    @current_line = line
+  end
+
+  private
+  
+  def error_message(message, serverity)
+    @errors.push(ErrorFile.new(@current_line_index, message, serverity))
+  end
+
 end
